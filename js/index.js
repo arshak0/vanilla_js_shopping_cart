5bb40cf7a4da3113c8eafc6982a570c045b516ad
arshak0 <arshak.ishkhanyan@gmail.com>
2020-11-23 12:58:00 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,65 +1,60 @@
 var app_state = {
-    goku_price: 5,
-    naruto_price: 20,
-    luffy_price: 7.5,
-    goku_count: 2,
-    naruto_count: 3,
-    luffy_count: 0,
-    goku_discount: -5,
-    naruto_discount: -3,
+    price: [5, 20, 7.5],
+    count: [2, 3, 0],
+    discount: [-5, -3],
     luffy_change: function( change_var ) {
-        this.luffy_count += change_var;
+        this.count[2] += change_var;
 
-        var summary_items = this.goku_count + this.naruto_count + this.luffy_count;
-        var summary_price = this.goku_count*this.goku_price + this.naruto_count*this.naruto_price + this.luffy_count*this.luffy_price;
-        var total_cost = summary_price + this.goku_discount + this.naruto_discount;
+        var summary_items = this.count[0] + this.count[1] + this.count[2];
+        var summary_price = this.count[0]*this.price[0] + this.count[1]*this.price[1] + this.count[2]*this.price[2];
+        var total_cost = summary_price + this.discount[0] + this.discount[1];
         update_html(summary_items, summary_price, total_cost);
     },
     naruto_change: function( change_var ) {
-        this.naruto_count += change_var;
-        if ( this.naruto_count >= 3 ) {
-            this.naruto_discount = - this.naruto_count;
+        this.count[1] += change_var;
+        if ( this.count[1] >= 3 ) {
+            this.discount[1] = - this.count[1];
         }
         else {
-            this.naruto_discount = 0;
+            this.discount[1] = 0;
         }
 
-        var summary_items = this.goku_count + this.naruto_count + this.luffy_count;
-        var summary_price = this.goku_count*this.goku_price + this.naruto_count*this.naruto_price + this.luffy_count*this.luffy_price;
-        var total_cost = summary_price + this.goku_discount + this.naruto_discount;
+        var summary_items = this.count[0] + this.count[1] + this.count[2];
+        var summary_price = this.count[0]*this.price[0] + this.count[1]*this.price[1] + this.count[2]*this.price[2];
+        var total_cost = summary_price + this.discount[0] + this.discount[1];
         update_html(summary_items, summary_price, total_cost);
     },
     goku_change: function(change_var) {
-        this.goku_count += change_var;
+        this.count[0] += change_var;
 
-        if ( this.goku_count >= 2 ) {
-            this.goku_discount = - (Math.floor(this.goku_count/2)*this.goku_price);
+        if ( this.count[0] >= 2 ) {
+            this.discount[0] = - (Math.floor(this.count[0]/2)*this.price[0]);
         }
         else {
-            this.goku_discount = 0;
+            this.discount[0] = 0;
         }
 
-        var summary_items = this.goku_count + this.naruto_count + this.luffy_count;
-        var summary_price = this.goku_count*this.goku_price + this.naruto_count*this.naruto_price + this.luffy_count*this.luffy_price;
-        var total_cost = summary_price + this.goku_discount + this.naruto_discount;
+        var summary_items = this.count[0] + this.count[1] + this.count[2];
+        var summary_price = this.count[0]*this.price[0] + this.count[1]*this.price[1] + this.count[2]*this.price[2];
+        var total_cost = summary_price + this.discount[0] + this.discount[1];
         update_html(summary_items, summary_price, total_cost);
     },
 };
 
 function update_html ( summary_items, summary_price, total_cost ) {
     //The function for updating the HTML elements
-    document.getElementsByClassName('item_quantity')[0].textContent=app_state.goku_count;
-    document.getElementsByClassName('item_quantity')[1].textContent=app_state.naruto_count;
-    document.getElementsByClassName('item_quantity')[2].textContent=app_state.luffy_count;
+    document.getElementsByClassName('item_quantity')[0].textContent=app_state.count[0];
+    document.getElementsByClassName('item_quantity')[1].textContent=app_state.count[1];
+    document.getElementsByClassName('item_quantity')[2].textContent=app_state.count[2];
     document.getElementsByClassName('summary_items_span')[0].textContent=summary_items;
     document.getElementsByClassName('summary_price_span')[0].textContent=summary_price;
-    document.getElementsByClassName('goku_discount_span')[0].textContent=app_state.goku_discount;
-    document.getElementsByClassName('naruto_discount_span')[0].textContent=app_state.naruto_discount;
+    document.getElementsByClassName('goku_discount_span')[0].textContent=app_state.discount[0];
+    document.getElementsByClassName('naruto_discount_span')[0].textContent=app_state.discount[1];
     document.getElementsByClassName('total_cost_span')[0].textContent=total_cost;
 
-    document.getElementsByClassName('item_price_span')[0].textContent=app_state.goku_count*app_state.goku_price;
-    document.getElementsByClassName('item_price_span')[1].textContent=app_state.naruto_count*app_state.naruto_price;
-    document.getElementsByClassName('item_price_span')[2].textContent=app_state.luffy_count*app_state.luffy_price;
+    document.getElementsByClassName('item_price_span')[0].textContent=app_state.count[0]*app_state.price[0];
+    document.getElementsByClassName('item_price_span')[1].textContent=app_state.count[1]*app_state.price[1];
+    document.getElementsByClassName('item_price_span')[2].textContent=app_state.count[2]*app_state.price[2];
 }
 
 
